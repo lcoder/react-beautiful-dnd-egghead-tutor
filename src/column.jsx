3ab65d1def2ad882a6e1b260935802d65ceebb73
毛庭峰 <mtingfeng@gmail.com>
2019-07-25 11:27:37 +0800

    初始数据

---
+++ src/column.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+    margin: 8px;
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+`
+const Title = styled.h3`
+    padding: 8px;
+`
+const TaskList = styled.div`
+    padding: 8px;
+`
+
+
+export default class Column extends React.Component {
+    render(){
+        return <Container>
+            <Title>{ this.props.column.title }</Title>
+            <TaskList>Tasks go here</TaskList>
+        </Container>
+    }
+}
