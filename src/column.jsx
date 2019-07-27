db349dc1f21f0450699d6ce20aadb3dbb42e86d1
毛庭峰 <mtingfeng@gmail.com>
2019-07-27 16:24:44 +0800

    简单拖拽

--- src/column.jsx
+++ src/column.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import styled from "styled-components"
+import Task from "./task"
+import { Droppable } from "react-beautiful-dnd"
 
 const Container = styled.div`
     margin: 8px;
@@ -18,7 +20,27 @@ export default class Column extends React.Component {
     render(){
         return <Container>
             <Title>{ this.props.column.title }</Title>
-            <TaskList>Tasks go here</TaskList>
+            <Droppable
+                droppableId={ this.props.column.id }
+            >
+                {
+                    ( provided ) => (
+                        <TaskList
+                            ref={ provided.innerRef }
+                            {...provided.droppableProps}
+                        >
+                            {
+                                this.props.tasks.map( ( task , index ) => (
+                                    <Task key={ task.id } task={ task } index={ index } />
+                                ) )
+                            }
+                            { provided.placeholder }
+                        </TaskList> 
+                    )
+                }
+            </Droppable>
         </Container>
     }
 }
+
+
